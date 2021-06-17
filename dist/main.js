26bc783292f7d5562b2df0136c48abc344b03dce
Rafael Trindade <rafaeltrindade649@gmail.com>
2021-06-16 21:51:29 -0300

    add swagger

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
+const swagger_1 = require("@nestjs/swagger");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.setGlobalPrefix('api');
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle("Cocobambu")
+        .setDescription("API de receitas da cozinha do cocobambu")
+        .setVersion("1.0")
+        .addTag("receitas")
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup("api", app, document);
     await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api");
+  const config = new DocumentBuilder()
+    .setTitle("Cocobambu")
+    .setDescription("API de receitas da cozinha do cocobambu")
+    .setVersion("1.0")
+    .addTag("receitas")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
