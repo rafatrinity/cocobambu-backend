122c37303496eb80c06a853ee64796642eb8b14e
Rafael Trindade <rafaeltrindade649@gmail.com>
2021-06-21 22:57:56 -0300

    Access-Control-Allow-Origin: *

--- src/receitas/receitas.controller.ts
+++ src/receitas/receitas.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Header } from '@nestjs/common';
 import { ReceitasService } from './receitas.service';
 import { CreateReceitaDto } from './dto/create-receita.dto';
 import { UpdateReceitaDto } from './dto/update-receita.dto';
 import { Receita } from './entities/receita.entity';
 import { ApiBody } from '@nestjs/swagger';
+
 
 @Controller('receitas')
 export class ReceitasController {
@@ -17,6 +18,7 @@ export class ReceitasController {
 
   @ApiBody({type: CreateReceitaDto})
   @Get()
+  @Header('Access-Control-Allow-Origin', '*')
   async findAll(): Promise<Receita[]> {
     return this.receitasService.findAll();
   }
